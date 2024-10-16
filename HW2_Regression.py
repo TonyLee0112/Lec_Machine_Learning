06e29eb5cdcd1f74e7a440274d8cb23b6ed8d3ac
TonyLee0112 <115335757+TonyLee0112@users.noreply.github.com>
2024-10-16 12:57:47 +0900

    Update and rename HW2_Question1.py to HW2_Regression.py

--- HW2_Regression.py
+++ HW2_Regression.py
@@ -1,12 +1,13 @@
-# Polynomial Regression
-# Train_set : 40 %, Test_set : 60 %
-# Verification_set : 10 % of Train_set
+# Polynomial Regression 문제
 
 # import libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error
 
 # load data
 # data.shape = (1999,2)
@@ -28,11 +29,7 @@ sub_input, val_input, sub_target, val_target = train_test_split(train_input,trai
 
 # 1. Polynomial 의 order 를 1~10차까지 증가시키며 각 order 에서의 MSE performance 를 계산,
 # MSE 가 가장 낮은 최적의 order 를 찾아라.
-
 MSE_scores = []
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.metrics import mean_squared_error
 for order in range(1,11) :
     poly = PolynomialFeatures(degree=order)
     poly_input = poly.fit_transform(sub_input) # 0차부터 ~ order 차 까지 데이터 생성
@@ -49,5 +46,30 @@ for order in range(1,11) :
     mse = mean_squared_error(val_target,val_predict)
     MSE_scores.append(mse)
 
-opt_order = np.argmin(MSE_scores) + 1
-print(f"최적의 차수는 : {opt_order}")
+opt_order = np.argmin(MSE_scores) + 1 # opt_order = 9
+# print(f"최적의 차수는 : {opt_order}")
+
+# 2. 최적의 Polynomial order 이 구해졌으면, 이를 이용하여 Train_set 을 초록색 실선으로
+# Plot 해보고, 그 그래프 위에 Test_data_set 을 파란색 점으로 overlay 시켜보아라.
+
+# 다항 특징 생성
+poly = PolynomialFeatures(degree=opt_order)
+poly_train_input = poly.fit_transform(train_input)
+poly_test_input = poly.transform(test_input)
+
+
+lr = LinearRegression()
+lr.fit(poly_train_input,train_target)
+
+train_predict = lr.predict(poly_train_input)
+
+# Train_set 의 예측값을 초록색 실선으로 그리기
+plt.scatter(train_input,train_predict,color ='green',label=f'train_set Order ={opt_order}')
+# Test_set 을 파란색 점으로 표시
+plt.scatter(test_input,test_target,color='blue',label ='Test_set', marker='.')
+
+plt.title(f'Optimal Prediction with Opt_order {opt_order}')
+plt.xlabel('Input')
+plt.ylabel('y')
+plt.legend() # scatter label 표시
+plt.show()
